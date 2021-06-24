05380338b57f3ea32d6eb8f6ea2ad1cdd39441aa
Zachary Wade <zacharywade@gmail.com>
2021-06-23 21:23:27 -0400

    First attempt gh-pages

--- src/shaders/shaders.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "*.glsl" {
-    import type { GlslShader } from "webpack-glsl-minify";
-
-    const value: GlslShader;
-    export default value;
-}
--- src/webgl-utils/index.ts
+++
@@ -1,5 +0,0 @@
-export { UniformKind as AttrKind, UniformKindToValue as AttrKindToValue, Uniforms as Attrs, UniformValues as AttrValues } from "./uniform";
-export { Faery } from "./faery";
-export { Geometry, Triangles } from "./geometry";
-export { Program } from "./program";
-export { WatnessCanvas } from "./watness-canvas";
